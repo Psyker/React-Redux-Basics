14bf589abd6e9ad6f890092373614b87410de0f7
Psyker <bourgoin.theo1@gmail.com>
2017-03-25 18:48:25 +0100

    create react tic-ta-toe

---
+++ React-Basics/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Square from './Square';
+import Board from './Board';
+import Game from './Game';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('container')
+);
